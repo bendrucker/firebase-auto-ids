7465c40ff3e0f3fab1a55b2549760ef0773b6075
Ben Drucker <bvdrucker@gmail.com>
2018-11-14 08:53:51 -0800

    standard --fix

--- src/generator.js
+++ src/generator.js
@@ -1,52 +1,51 @@
-'use strict';
+'use strict'
 
-var allowedCharacters = '-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz';
+var allowedCharacters = '-0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz'
 // allowedCharacters.length = 64
 
 function IdGenerator () {
   this.run = {
     time: 0,
     randomCharacterIndexes: []
-  };
+  }
 }
 
 IdGenerator.prototype.time = function () {
-  var length = 8;
-  var characters = new Array(length);
-  var now = this.run.time;
+  var length = 8
+  var characters = new Array(length)
+  var now = this.run.time
   for (var i = length - 1; i >= 0; i--) {
-    characters[i] = allowedCharacters.charAt(now % 64);
-    now = Math.floor(now / 64);
+    characters[i] = allowedCharacters.charAt(now % 64)
+    now = Math.floor(now / 64)
   }
-  return characters.join('');
-};
+  return characters.join('')
+}
 
 IdGenerator.prototype.random = function (sequential) {
-  var indexes = this.run.randomCharacterIndexes;
-  var i;
+  var indexes = this.run.randomCharacterIndexes
+  var i
   if (!sequential) {
     for (i = 0; i < 12; i++) {
-      indexes[i] = Math.floor(Math.random() * 64);
+      indexes[i] = Math.floor(Math.random() * 64)
     }
-  }
-  else {
+  } else {
     for (i = 11; i >= 0 && indexes[i] === 63; i--) {
-      indexes[i] = 0;
+      indexes[i] = 0
     }
-    indexes[i]++;
+    indexes[i]++
   }
   return indexes.map(function (index) {
-    return allowedCharacters[index];
+    return allowedCharacters[index]
   })
-  .reduce(function (id, character) {
-    return id + character;
-  }, '');
-};
+    .reduce(function (id, character) {
+      return id + character
+    }, '')
+}
 
 IdGenerator.prototype.generate = function (now) {
-  var uniqueTime = now !== this.run.time;
-  this.run.time = now;
-  return this.time() + this.random(!uniqueTime);
-};
+  var uniqueTime = now !== this.run.time
+  this.run.time = now
+  return this.time() + this.random(!uniqueTime)
+}
 
-module.exports = IdGenerator;
+module.exports = IdGenerator
--- test/index.js
+++ test/index.js
@@ -1,40 +1,38 @@
-'use strict';
+'use strict'
 
-var generateAutoId = require('../');
-var test       = require('tape');
-var sinon      = require('sinon');
+var generateAutoId = require('../')
+var test = require('tape')
+var sinon = require('sinon')
 
 function id () {
-  return generateAutoId(new Date().getTime());
+  return generateAutoId(new Date().getTime())
 }
 
-
 test('general properties', function (t) {
-  t.ok(id().length === 20, 'length is 20');
-  t.end();
-});
+  t.ok(id().length === 20, 'length is 20')
+  t.end()
+})
 
 test('time ordering', function (t) {
-  var clock = sinon.useFakeTimers();
-  var id1 = id();
-  clock.tick(1);
-  var id2 = id();
-  clock.tick(1);
-  var id3 = id();
-  t.ok(id1 < id2);
-  t.ok(id2 < id3);
-  clock.restore();
-  t.end();
-});
+  var clock = sinon.useFakeTimers()
+  var id1 = id()
+  clock.tick(1)
+  var id2 = id()
+  clock.tick(1)
+  var id3 = id()
+  t.ok(id1 < id2)
+  t.ok(id2 < id3)
+  clock.restore()
+  t.end()
+})
 
 test('call ordering', function (t) {
-  var clock = sinon.useFakeTimers();
-  var id1 = id();
-  var id2 = id();
-  var id3 = id();
-  t.ok(id1 < id2);
-  t.ok(id2 < id3);
-  clock.restore();
-  t.end();
-});
-
+  var clock = sinon.useFakeTimers()
+  var id1 = id()
+  var id2 = id()
+  var id3 = id()
+  t.ok(id1 < id2)
+  t.ok(id2 < id3)
+  clock.restore()
+  t.end()
+})
